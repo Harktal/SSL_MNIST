ec0503d5d30a9fa14645b088c1f3c8ec47980b0c
Harktal <thibaud.riehm.sco@gmail.com>
2019-11-06 16:01:43 +0100

    Commit de la première version publique

---
+++ lib/paths.py
@@ -0,0 +1,9 @@
+"""Dataset paths used in open-source version of this codebase."""
+TRAIN_DIR = "/content/experiments_folder/FirstTest/Map"
+IMAGENET_DATA_DIR = "./data/imagenet-2012-tfrecord"
+IMAGENET_32_DATA_DIR = "./data/imagenet_32"
+
+BUILD_TFRECORDS_DOWNLOAD_PATH = "/content/experiments_folder/FirstTest/Map"
+BUILD_TFRECORDS_DATA_PREFIX = "/content/experiments_folder/FirstTest/Map"
+RAW_IMAGENET_PATH = "./data/raw/imagenet/"
+LABEL_MAP_PATH = "/content/experiments_folder/FirstTest/Map"
---
+++ lib/tf_utils.py
@@ -0,0 +1,52 @@
+"""A set of small TF utility functions shared between train and evaluate."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+
+
+def hash_float(x, big_num=1000 * 1000):
+    """Hash a tensor 'x' into a floating point number in the range [0, 1)."""
+    return tf.cast(
+        tf.string_to_hash_bucket_fast(x, big_num), tf.float32
+    ) / tf.constant(float(big_num))
+
+
+def make_set_filter_fn(elements):
+    """Constructs a TensorFlow "set" data structure.
+    Note that sets returned by this function are uninitialized. Initialize them
+    by calling `sess.run(tf.tables_initializer())`
+    Args:
+        elements: A list of non-Tensor elements.
+    Returns:
+        A function that when called with a single tensor argument, returns
+        a boolean tensor if the argument is in the set.
+    """
+    table = tf.contrib.lookup.HashTable(
+        tf.contrib.lookup.KeyValueTensorInitializer(
+            elements, tf.tile([1], [len(elements)])
+        ),
+        default_value=0,
+    )
+
+    return lambda x: tf.equal(table.lookup(x), 1)
+
+
+def filter_fn_from_comma_delimited(string):
+    """Parses a string of comma delimited numbers, returning a filter function.
+    This utility function is useful for parsing flags that represent a set of
+    options, where the default is "all options".
+    Args:
+        string: e.g. "1,2,3", or empty string for "set of all elements"
+    Returns:
+        A function that when called with a single tensor argument, returns
+        a boolean tensor that evaluates to True if the argument is in the set.
+        If 'string' argument is None, the set is understood to contain all
+        elements and the function always returns a True tensor.
+    """
+    if string:
+        return make_set_filter_fn(list(map(int, string.split(","))))
+    else:
+        return lambda x: tf.constant(True)
